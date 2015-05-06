b8af9ed3e8dfe291c01c9fc32a896f2af87d1d10
Dominic LaRoche <dlaroche@email.arizona.edu>
2015-05-06 10:14:08 -0700

    Changed highlighting of x-axis and created different versions od the plot with three different text size

--- VariableImportancePlot.R
+++ VariableImportancePlot.R
@@ -13,4 +13,9 @@ csflong$imp<-ifelse(csflong$Coefficient==0,"No","Yes")
 names(csflong)[1]<-"Variable"
 library(ggplot2)
 
-ggplot(data=csflong[csflong$Variable!="(Intercept)",],aes(x=Variable,y=Coefficient))+geom_point(size=3, aes(color=imp))+geom_hline(yintercept=0) +ylab("Linear Coefficient")+scale_color_manual("Non-zero\nCoefficient", values=c("red","blue"))+facet_wrap(~Type, ncol=1)+ theme(axis.text.x=element_text(angle=-50, hjust=0))
+#map axis text to colors can only figure out how to do this manually at this point
+myColors<-c(rep("black",9),rep("blue",10), rep("black",1), rep("blue",3), "black", "blue", "black", "blue", rep("black",2), rep("blue",4), rep("black",2), "blue")
+cfs<-cfs[order(cfs$X),]
+myColors<-ifelse(abs(cfs$Grouped.Coef)>.1,"blue","black")
+
+ggplot(data=csflong[csflong$Variable!="(Intercept)" & csflong$Type=="Grouped LASSO",],aes(x=Variable,y=Coefficient))+geom_point(size=3, aes(color=imp))+geom_hline(yintercept=0) +ylab("Penalized Linear Coefficient")+scale_color_manual("Non-zero\nCoefficient", values=c("red","blue"))+ theme(axis.text.x=element_text(angle=-50, hjust=0, color=myColors[-1], size=6))
